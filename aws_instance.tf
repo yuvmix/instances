0bc6d0794ecd290cfb633c0a9830c266fb55ee5d
yuvmix <113332005+yuvmix@users.noreply.github.com>
2022-12-19 08:23:56 +0200

    Create aws_instance.tf

---
+++ aws_instance.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-019f0583716f0aec4" 
+  instance_type = "t2.micro"
+  key_name = "key.pem"
+  security_group= ["ssh_security_group"]
+  
+  tags = {
+    Name = "visualscrapy"
+  }
+}
