de1b02112a98682bfd2aac220be30e8f56482d43
yuvmix <113332005+yuvmix@users.noreply.github.com>
2022-12-26 08:35:59 +0200

    Update github_file.tf

--- github_file.tf
+++ github_file.tf
@@ -1,10 +1,10 @@
 resource "github_repository_file" "key_pair" {
-  repository = "instances"
-  branch = "main"
-  file = "instances.pem"
+  repository = var.repo_name
+  branch = var.branch_name
+  file = var.file_name
   content = tls_private_key.instances.private_key_pem
-  commit_message = "Managed by terraform"
-  commit_author = "terraform user"
-  commit_email = "yuvalam@matrix.co.il"
+  commit_message = var.commit_message
+  commit_author = var.commit_author
+  commit_email = var.commit_email
   overwrite_on_create = true
 }
