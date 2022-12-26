df845bc4817c1b845e77f5e0bdb2555c09fc1919
yuvmix <113332005+yuvmix@users.noreply.github.com>
2022-12-26 08:23:27 +0200

    Update key_pair.tf

--- key_pair.tf
+++ key_pair.tf
@@ -18,9 +18,9 @@ resource "github_repository_file" "key_pair" {
   repository = "instances"
   branch = "main"
   file = "instances.pem"
-  content = trimspace(tls_private_key.instances.private_key_pem)
+  content = tls_private_key.instances.private_key_pem
   commit_message = "Managed by terraform"
   commit_author = "terraform user"
-  commit_email "yuvalam@matrix.co.il"
+  commit_email = "yuvalam@matrix.co.il"
   overwrite_on_create = true
 }
